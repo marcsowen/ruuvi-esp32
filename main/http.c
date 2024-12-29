5cdb01e52517912557f577971867183c5afda5e4
Marc Sowen <marc@sowen.net>
2024-12-29 16:24:28 +0100

    Added esp_mac and JSON numbers whenever possible

--- main/http.c
+++ main/http.c
@@ -13,6 +13,7 @@ static const char *TAG = "ruuvi-esp32-http";
 
 measurement_t measurements[MAX_SENSORS];
 int sensor_count = 0;
+char esp_id[18];
 
 void update_sensor(measurement_t new_measurement) {
     for (int i = 0; i < sensor_count; i++) {
@@ -53,10 +54,12 @@ static esp_err_t request_handler(httpd_req_t *req) {
 
     offset += snprintf(response + offset, JSON_BUFFER_SIZE - offset,
         "{"
-        "\"timestamp\": \"%lld\","
+        "\"esp_mac\": \"%s\","
+        "\"timestamp\": %lld,"
         "\"bssid\": \"%s\","
-        "\"wifi_rssi\": \"%d\","
+        "\"wifi_rssi\": %d,"
         " \"sensors\": [",
+        esp_id,
         time(NULL),
         bssid,
         wifi_rssi
@@ -66,20 +69,20 @@ static esp_err_t request_handler(httpd_req_t *req) {
         measurement_t* entry = &measurements[i];
         offset += snprintf(response + offset, JSON_BUFFER_SIZE - offset,
             "{"
-            "\"mac_address\": \"%s\","
-            "\"timestamp\": \"%lld\","
+            "\"ble_mac\": \"%s\","
+            "\"timestamp\": %lld,"
             "\"name\": \"%s\","
-            "\"ble_rssi\": \"%d\","
-            "\"temperature\": \"%.2f\","
-            "\"humidity\": \"%.2f\","
-            "\"pressure\": \"%.2f\","
-            "\"acceleration_x\": \"%.3f\","
-            "\"acceleration_y\": \"%.3f\","
-            "\"acceleration_z\": \"%.3f\","
-            "\"battery\": \"%.3f\","
-            "\"tx_power\": \"%d\","
-            "\"moves\": \"%d\","
-            "\"sequence\": \"%d\""
+            "\"ble_rssi\": %d,"
+            "\"temperature\": %.2f,"
+            "\"humidity\": %.2f,"
+            "\"pressure\": %.2f,"
+            "\"acceleration_x\": %.3f,"
+            "\"acceleration_y\": %.3f,"
+            "\"acceleration_z\": %.3f,"
+            "\"battery\": %.3f,"
+            "\"tx_power\": %d,"
+            "\"moves\": %d,"
+            "\"sequence\": %d"
             "}",
             entry->bda,
             entry->timestamp,
@@ -109,6 +112,12 @@ static esp_err_t request_handler(httpd_req_t *req) {
 }
 
 httpd_handle_t start_webserver(void) {
+
+    uint8_t mac[6];
+    ESP_ERROR_CHECK(esp_wifi_get_mac(ESP_IF_WIFI_STA, mac));
+    sprintf(esp_id, "%02X:%02X:%02X:%02X:%02X:%02X",
+        mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+
     const httpd_config_t config = HTTPD_DEFAULT_CONFIG();
 
     httpd_handle_t server = NULL;
